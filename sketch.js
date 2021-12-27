d4499cfa6cf25aaf83816e920d13cb1217436276
OphiliaFradarick <76613993+OphiliaFradarick@users.noreply.github.com>
2021-12-27 18:31:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var box;
+
+function setup(){
+  createCanvas(1500,800);
+
+  fill("orange");
+  wall1 = createSprite(50, 500, 10, 400);
+  wall1 = createSprite(200, 550, 10, 300);
+  wall1 = createSprite(345, 300, 600, 10);
+  wall1 = createSprite(495, 400, 600, 10);
+  wall1 = createSprite(640,155, 10, 300);
+  wall1 = createSprite(790,255, 10, 300);
+  wall1 = createSprite();
+  wall1 = createSprite();
+  wall1 = createSprite();
+  wall1 = createSprite();
+  wall1 = createSprite();
+  wall1 = createSprite();
+  wall1 = createSprite();
+  wall1 = createSprite();
+
+
+
+ 
+}
+
+function draw(){
+  background('black');
+
+  
+
+  drawSprites();
+
+}
